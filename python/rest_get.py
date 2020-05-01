191b9eb349f215d46f4143388d0d5d69a367bdcc
m.schmelter <m.schmelter@grolman-group.com>
2020-05-01 21:38:50 +0200

    -

--- python/rest_get.py
+++ python/rest_get.py
@@ -2,14 +2,14 @@
 import requests
 
 # Varibles
-destination_url = 'localhost'
+destination_url = ''
 destination_port = 2375
 resource_path = 'info'
 
 
 # Functions
 def rest_api_get(active_url, active_port, active_resource):
-    resource_path = "http://%s:%s/%s" % (active_url, active_port, active_resource)
+    resource_path = "https://%s:%s/%s" % (active_url, active_port, active_resource)
 
     rest_response = requests.get(url=resource_path)
     return rest_response.json()
